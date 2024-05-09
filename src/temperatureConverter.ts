f3fef6e0822489bf701b8938455aaf332c1bf5bb
Sam <sam124674@gmail.com>
2024-05-09 16:09:14 +0800

    fix

---
+++ src/temperatureConverter.ts
@@ -0,0 +1,9 @@
+/**
+ * 將攝氏溫度轉換為華氏溫度
+ * @param celsius - 攝氏溫度
+ * @returns 華氏溫度
+ */
+export function celsiusToFahrenheit(celsius: number): number {
+  let ans = celsius * 9 / 5 + 32
+  return  ans
+}
