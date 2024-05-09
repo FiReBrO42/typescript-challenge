5fc738deae0f8152cb519df34ed7c8316f33f0ed
Sam <sam124674@gmail.com>
2024-05-09 16:36:01 +0800

    set ; 2

--- src/arraySum.ts
+++ src/arraySum.ts
@@ -15,7 +15,7 @@
 export function arraySum(numbers: number[]): number {
     // 在此實現函式
     if(numbers.length == 0){
-        return 0
+        return 0;
     }
     let ans:number = 0
     numbers.forEach((num)=>{
